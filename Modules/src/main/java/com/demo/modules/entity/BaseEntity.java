e5c72c711b5ad1088b9fbf19aafd7702062111dd
SULLDONGBI <sdb7734@gmail.com>
2024-03-21 14:13:33 +0900

    기본 엔티티 추가 / 모듈 실행 파일 삭제

---
+++ Modules/src/main/java/com/demo/modules/entity/BaseEntity.java
@@ -0,0 +1,41 @@
+package com.demo.modules.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@DynamicInsert
+@DynamicUpdate
+@Getter
+@Setter
+public class BaseEntity extends AuditingEntityListener {
+    @CreatedBy
+    @Column(name = "created_by", nullable = false, updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(name = "last_modified_by")
+    private String lastModifiedBy;
+
+    @CreatedDate
+    @Column(name = "created_date", nullable = false, updatable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_date")
+    private LocalDateTime lastModifiedDate;
+
+    @Column(name = "delete_yn", columnDefinition = "varchar(1) default 'N'")
+    private String deleteYn;
+}
