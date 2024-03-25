c1fd71da276a3349cc6f906ae6733175be910e0c
SULLDONGBI <sdb7734@gmail.com>
2024-03-25 10:33:00 +0900

    api 모듈 엔티티 / 레포지토리 찾을 수 있도록 Scan 위치 지정

--- Api/src/main/java/com/demo/api/ApiApplication.java
+++ Api/src/main/java/com/demo/api/ApiApplication.java
@@ -2,8 +2,12 @@ package com.demo.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.demo.modules.repository") // JPA Repository 패키지 경로 지정
+@EntityScan("com.demo.modules.entity") // 엔티티 클래스 패키지 경로 지정
 public class ApiApplication {
 
     public static void main(String[] args) {
