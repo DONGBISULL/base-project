ffa739d2938f35b0dd6b93624cd75797120b11ff
SULLDONGBI <sdb7734@gmail.com>
2024-03-27 17:03:55 +0900

    컴포넌트 스캔 위한 패키지 위치 지정

--- Api/src/main/java/com/demo/api/ApiApplication.java
+++ Api/src/main/java/com/demo/api/ApiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.demo.modules.*" , "com.demo.api.*"})
 @EnableJpaRepositories("com.demo.modules.repository") // JPA Repository 패키지 경로 지정
 @EntityScan("com.demo.modules.entity") // 엔티티 클래스 패키지 경로 지정
 public class ApiApplication {
