2cea56afddcd3e1306cefef284a9f624917df004
SULLDONGBI <sdb7734@gmail.com>
2024-03-29 11:41:49 +0900

    repository 주석 추가

--- Modules/src/main/java/com/demo/modules/repository/MemberRepository.java
+++ Modules/src/main/java/com/demo/modules/repository/MemberRepository.java
@@ -7,5 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
+
+    /* 소셜 로그인한 회원 소셜 로그인 아이디와 로그인 매체로 회원 확인  */
     public Optional<Member> findBySocialIdAndProviderType(String socialId, ProviderType providerType);
 }
