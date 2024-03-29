d5e54523e4024103f4e5a09777ee25cb4f85216f
SULLDONGBI <sdb7734@gmail.com>
2024-03-29 11:40:15 +0900

    CustomOAuth2UserService 를 통해 로그인 정보 확인 로직 및 회원 조회 로직 추가

--- Api/src/main/java/com/demo/api/security/service/CustomOAuth2UserService.java
+++ Api/src/main/java/com/demo/api/security/service/CustomOAuth2UserService.java
@@ -1,7 +1,17 @@
 package com.demo.api.security.service;
 
+import com.demo.api.service.MemberService;
+import com.demo.modules.dto.OAuthAttribute;
+import com.demo.modules.dto.OAuthUserDto;
+import com.demo.modules.entity.Member;
+import com.demo.modules.enums.ProviderType;
+import com.demo.modules.enums.Role;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
@@ -10,17 +20,66 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
-public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User>   {
+public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final MemberService memberService;
+
+    @Transactional
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        log.debug("=========== loadUser ===============");
         log.trace("Load user {}", userRequest);
+
         ClientRegistration clientRegistration = userRequest.getClientRegistration();
-        System.out.println(userRequest);
-        System.out.println("=========== clientRegistration ===============");
-        System.out.println(clientRegistration);
-        return null;
+        log.debug("=========== clientRegistration  ===============");
+        log.debug(clientRegistration.toString());
+
+        log.debug("=========== oAuth2User  ===============");
+        OAuth2UserService<OAuth2UserRequest, OAuth2User> delegate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delegate.loadUser(userRequest);
+        log.debug(oAuth2User.toString());
+
+        Map<String, Object> attributes = oAuth2User.getAttributes();
+        ClientRegistration.ProviderDetails providerDetails = clientRegistration.getProviderDetails();
+        String userNameAttributeName = providerDetails.getUserInfoEndpoint().getUserNameAttributeName();
+        String registrationId = clientRegistration.getRegistrationId();
+        ProviderType provider = getProvider(registrationId);
+
+//        유저정보로 contirbute 생성
+        OAuthAttribute oAuthAttribute = OAuthAttribute.of(provider, userNameAttributeName, attributes);
+
+        Member member = memberService.getMember(oAuthAttribute, provider);
+
+        Set<Role> roles = member.getRoles();
+        Set<GrantedAuthority> authorities = roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toSet());
+        OAuthUserDto userDto = new OAuthUserDto(authorities, attributes, userNameAttributeName);
+        log.debug("=========== userDto  ===============");
+        log.debug(userDto.toString());
+        return userDto;
+    }
+
+    public ProviderType getProvider(String registrationId) {
+        if (registrationId == null) {
+            return null;
+        }
+        if ("kakao".equals(registrationId)) {
+            return ProviderType.KAKAO;
+        } else if ("naver".equals(registrationId)) {
+            return ProviderType.NAVER;
+        } else if ("google".equals(registrationId)) {
+            return ProviderType.GOOGLE;
+        } else {
+            return ProviderType.NOMAL;
+        }
     }
 }
