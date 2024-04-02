d0a7a1a4bd693773aafdd7904b653b97c2809d3b
SULLDONGBI <sdb7734@gmail.com>
2024-04-02 18:18:13 +0900

    순환 참조 구조로 인해 config 파일 분리

---
+++ Api/src/main/java/com/demo/api/config/OAuth2Config.java
@@ -0,0 +1,171 @@
+package com.demo.api.config;
+
+import com.demo.api.security.anotation.GoogleOAuth2Properties;
+import com.demo.api.security.filter.JwtTokenFilter;
+import com.demo.api.security.handler.OAuth2AuthenticationFailHandler;
+import com.demo.api.security.handler.OAuth2AuthenticationSuccessHandler;
+import com.demo.api.security.provider.JwtTokenProvider;
+import com.demo.api.security.service.CustomOAuth2UserService;
+import com.demo.modules.enums.CustomOAuth2Provider;
+import com.demo.modules.repository.TokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.client.InMemoryOAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.endpoint.NimbusAuthorizationCodeTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AccessTokenResponseClient;
+import org.springframework.security.oauth2.client.endpoint.OAuth2AuthorizationCodeGrantRequest;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.AuthenticatedPrincipalOAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Configuration
+@RequiredArgsConstructor
+public class OAuth2Config {
+
+    private final Environment env;
+    private final List<String> clients = Arrays.asList("kakao", "naver", "google");
+
+    private final CustomOAuth2UserService auth2UserService;
+
+    // 구글 스코프
+    private final GoogleOAuth2Properties googleScopes;
+
+    private final OAuth2AuthenticationFailHandler auth2AuthenticationFailHandler;
+
+    private final JwtTokenFilter jwtTokenFilter;
+
+    private final TokenRepository tokenRepository;
+
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests(authorize -> authorize
+                        .antMatchers("/oauth/**", "/login/**", "/images/**", "/favicon.ico/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(
+                        oauth2 -> oauth2
+                                .loginPage("/oauth/login")
+                                .clientRegistrationRepository(clientRegistrationRepository())
+                                .authorizedClientRepository(authorizedClientRepository())
+                                .authorizedClientService(auth2AuthorizedClientService())
+//                               카카오에서 설정한 rediection 으로 생각하면 됨 / 돌아오는 위치
+                                .redirectionEndpoint(
+                                        redirection ->
+                                                redirection
+                                                        .baseUri("/login/social/**")
+                                ).tokenEndpoint(tokenEndpoint -> tokenEndpoint // 토큰 엔드포인트 설정
+                                        .accessTokenResponseClient(accessTokenResponseClient()))
+                                .userInfoEndpoint(userInfo -> userInfo
+                                        .userService(auth2UserService)
+                                )
+                                .successHandler( // 인증 성공 시 Handler
+                                        new OAuth2AuthenticationSuccessHandler(auth2AuthorizedClientService(), jwtTokenProvider, tokenRepository)
+                                )
+                                .failureHandler( // 인증 실패 시 Handler
+                                        auth2AuthenticationFailHandler
+                                )
+                );
+
+        /* jwt 사용한다고 가정 시 */
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+//        http.addFilterBefore(
+//                jwtTokenFilter,
+//                UsernamePasswordAuthenticationFilter.class
+//        );
+
+        return http.build();
+    }
+
+
+    @Bean
+    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
+        return new NimbusAuthorizationCodeTokenResponseClient();
+    }
+
+    /*
+     * ClientRegistration 클라이언트 등록 정보 저장
+     *
+     * 인가 서버에 일차적으로 클라이언트 등록 정보의 일부를 검색하는 기능 제공
+     * 다른 웹 요청이 와도 OAuth2AuthorizedClient 유지 !
+     * */
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        List<ClientRegistration> registrations = clients.stream()
+                .map(c -> getRegistration(c))
+                .filter(registration -> registration != null)
+                .collect(Collectors.toList());
+        return new InMemoryClientRegistrationRepository(registrations);
+    }
+
+    //     리소스 자격을 부여하는 소유자와 authorized client 를 연결하는 목적 제공
+    //    Auth2AuthorizedClient 의 저장,조회, 삭제 관리
+    @Bean
+    public OAuth2AuthorizedClientService auth2AuthorizedClientService() {
+        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository());
+    }
+
+    //     OAuth2AuthorizedClient 정보를 계속 유지
+    //    사용자가 인증된 클라이언트 정보를 저장하는 역할
+    @Bean
+    public OAuth2AuthorizedClientRepository authorizedClientRepository() {
+        return new AuthenticatedPrincipalOAuth2AuthorizedClientRepository(auth2AuthorizedClientService());
+    }
+
+    private static String CLIENT_PROPERTY_KEY = "spring.security.oauth2.client.registration.";
+
+    private ClientRegistration getRegistration(String client) {
+        String clientId = env.getProperty(
+                CLIENT_PROPERTY_KEY + client + ".client-id");
+
+        if (clientId == null) {
+            return null;
+        }
+
+        // API Client Secret 불러오기
+        String clientSecret = env.getProperty(
+                CLIENT_PROPERTY_KEY + client + ".client-secret");
+
+        if ("google".equals(client)) {
+            return CustomOAuth2Provider.GOOGLE.getBuilder(client)
+                    .clientSecret(clientSecret)
+                    .clientId(clientId)
+                    .scope(googleScopes.getScope())
+                    .build();
+        }
+
+        if ("naver".equals(client)) {
+            return CustomOAuth2Provider.NAVER.getBuilder(client)
+                    .clientSecret(clientSecret)
+                    .clientId(clientId)
+                    .build();
+        }
+
+        if ("kakao".equals(client)) {
+            return CustomOAuth2Provider.KAKAO.getBuilder(client)
+                    .clientSecret(clientSecret)
+                    .clientId(clientId)
+                    .build();
+        }
+
+        return null;
+    }
+
+}
--- Api/src/main/java/com/demo/api/config/SecurityConfig.java
+++ Api/src/main/java/com/demo/api/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.demo.api.config;
 
 
+import com.demo.api.security.filter.JwtTokenFilter;
 import com.demo.api.security.handler.OAuth2AuthenticationFailHandler;
 import com.demo.api.security.handler.OAuth2AuthenticationSuccessHandler;
+import com.demo.api.security.provider.JwtTokenProvider;
 import com.demo.modules.enums.CustomOAuth2Provider;
 import com.demo.api.security.anotation.CorsProperties;
 import com.demo.api.security.anotation.GoogleOAuth2Properties;
@@ -13,12 +15,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.client.*;
 import org.springframework.security.oauth2.client.endpoint.NimbusAuthorizationCodeTokenResponseClient;
@@ -29,10 +31,7 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.AuthenticatedPrincipalOAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
-import org.springframework.security.oauth2.client.web.OAuth2LoginAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -46,155 +45,7 @@ import java.util.stream.Collectors;
 @Slf4j
 public class SecurityConfig {
 
-// Authorization Code with PKCE 이 방법으로 구현하도록 하자!
-
     private final CorsProperties corsProperties;
-
-    private final Environment env;
-
-    private final List<String> clients = Arrays.asList("kakao", "naver", "google");
-
-    private final CustomOAuth2UserService auth2UserService;
-
-    // 구글 스코프
-    private final GoogleOAuth2Properties googleScopes;
-
-//    private final OAuth2AuthenticationSuccessHandler auth2AuthenticationSuccessHandler;
-
-    private final OAuth2AuthenticationFailHandler auth2AuthenticationFailHandler;
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public UserDetailsService customUserDetailsService() {
-        return new CustomUserDetailsService(); // CustomUserDetailsService는 UserDetailsService 인터페이스를 구현한 클래스입니다.
-    }
-
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests(authorize -> authorize
-                        .antMatchers("/oauth/**", "/login/**", "/images/**", "/favicon.ico/**").permitAll()
-                        .anyRequest().authenticated()
-                )
-                .oauth2Login(
-                        oauth2 -> oauth2
-                                .loginPage("/oauth/login")
-                                .clientRegistrationRepository(clientRegistrationRepository())
-                                .authorizedClientRepository(authorizedClientRepository())
-                                .authorizedClientService(auth2AuthorizedClientService())
-//                               카카오에서 설정한 rediection 으로 생각하면 됨 / 돌아오는 위치
-                                .redirectionEndpoint(
-                                        redirection ->
-                                                redirection
-                                                        .baseUri("/login/social/**")
-                                ).tokenEndpoint(tokenEndpoint -> tokenEndpoint // 토큰 엔드포인트 설정
-                                        .accessTokenResponseClient(accessTokenResponseClient()))
-                                .userInfoEndpoint(userInfo -> userInfo
-                                        .userService(auth2UserService)
-                                )
-                                .successHandler( // // 인증 성공 시 Handler
-                                        new OAuth2AuthenticationSuccessHandler(auth2AuthorizedClientService())
-                                )
-                                .failureHandler( // 인증 실패 시 Handler
-                                        auth2AuthenticationFailHandler
-                                )
-//                                .defaultSuccessUrl("/")
-
-//                              .failureUrl("/auth/error");
-                );
-        return http.build();
-    }
-
-
-    //                .oauth2Client(oauth2Client -> oauth2Client
-//                                .clientRegistrationRepository(clientRegistrationRepository())
-////                        authorizedClientRepository   사용자가 인증된 클라이언트 정보를 저장하는 역할
-//                                .authorizedClientRepository(authorizedClientRepository())
-////                        리소스 자격을 부여하는 소유자와 authorized client 를 연결하는 목적 제공
-//                                .authorizedClientService(oauth2AuthorizedClientService())
-//                ).
-//                .addFilterBefore(customOAuth2LoginAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-
-
-    @Bean
-    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
-        return new NimbusAuthorizationCodeTokenResponseClient();
-    }
-
-    /*
-     * ClientRegistration 클라이언트 등록 정보 저장
-     *
-     * 인가 서버에 일차적으로 클라이언트 등록 정보의 일부를 검색하는 기능 제공
-     * 다른 웹 요청이 와도 OAuth2AuthorizedClient 유지 !
-     * */
-    @Bean
-    public ClientRegistrationRepository clientRegistrationRepository() {
-        List<ClientRegistration> registrations = clients.stream()
-                .map(c -> getRegistration(c))
-                .filter(registration -> registration != null)
-                .collect(Collectors.toList());
-        return new InMemoryClientRegistrationRepository(registrations);
-    }
-
-    //    Auth2AuthorizedClient 의 저장,조회, 삭제 관리
-    @Bean
-    public OAuth2AuthorizedClientService auth2AuthorizedClientService() {
-        return new InMemoryOAuth2AuthorizedClientService(clientRegistrationRepository());
-    }
-
-    //    //     OAuth2AuthorizedClient 정보를 계속 유지
-    @Bean
-    public OAuth2AuthorizedClientRepository authorizedClientRepository() {
-        return new AuthenticatedPrincipalOAuth2AuthorizedClientRepository(auth2AuthorizedClientService());
-    }
-
-    private static String CLIENT_PROPERTY_KEY = "spring.security.oauth2.client.registration.";
-
-    private ClientRegistration getRegistration(String client) {
-        String clientId = env.getProperty(
-                CLIENT_PROPERTY_KEY + client + ".client-id");
-
-        if (clientId == null) {
-            return null;
-        }
-
-        // API Client Secret 불러오기
-        String clientSecret = env.getProperty(
-                CLIENT_PROPERTY_KEY + client + ".client-secret");
-
-        // Redirect URI 설정
-//        String redirectUri = env.getProperty(CLIENT_PROPERTY_KEY + client + ".redirect-uri");
-
-        if ("google".equals(client)) {
-            return CustomOAuth2Provider.GOOGLE.getBuilder(client)
-                    .clientSecret(clientSecret)
-                    .clientId(clientId)
-                    .scope(googleScopes.getScope())
-                    .build();
-        }
-
-        if ("naver".equals(client)) {
-            return CustomOAuth2Provider.NAVER.getBuilder(client)
-                    .clientSecret(clientSecret)
-                    .clientId(clientId)
-                    .build();
-        }
-
-        if ("kakao".equals(client)) {
-            return CustomOAuth2Provider.KAKAO.getBuilder(client)
-                    .clientSecret(clientSecret)
-                    .clientId(clientId)
-                    .build();
-        }
-
-        return null;
-    }
-
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
@@ -222,5 +73,14 @@ public class SecurityConfig {
         return source;
     }
 
+    @Bean
+    public UserDetailsService customUserDetailsService() {
+        return new CustomUserDetailsService(); // CustomUserDetailsService는 UserDetailsService 인터페이스를 구현한 클래스입니다.
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
