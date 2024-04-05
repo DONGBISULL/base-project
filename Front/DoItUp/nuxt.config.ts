fb3099d86dd4034c136fe6291677c3523f503290
SULLDONGBI <sdb7734@gmail.com>
2024-04-05 18:00:54 +0900

    프론트 임시 생성

---
+++ Front/DoItUp/nuxt.config.ts
@@ -0,0 +1,23 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+    devtools: {enabled: true},
+    runtimeConfig: {
+        // The private keys which are only available within server-side
+        // Keys within public, will be also exposed to the client-side
+        public: {
+            API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8080',
+            KAKAO_LOGIN_PREFIX: process.env.KAKAO_LOGIN_PREFIX || '/oauth2/authorization/kakao',
+            GOOGLE_LOGIN_PREFIX: process.env.GOOGLE_LOGIN_PREFIX || '/oauth2/authorization/google',
+        }
+    }
+    // vite: {
+    // server: {
+    //   proxy: {
+    //     "/api": {
+    //       target: "http://localhost:8080",
+    //       changeOrigin: true,
+    //     },
+    //   },
+    // },
+    // },
+})
