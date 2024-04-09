8ca54baf53ff18d94656f2c1f07cf5d8956e1837
SULLDONGBI <sdb7734@gmail.com>
2024-04-09 18:14:20 +0900

    프론트 임시 useFetch 추가

--- Front/DoItUp/pages/oauth/check.vue
+++ Front/DoItUp/pages/oauth/check.vue
@@ -9,9 +9,7 @@ const headers = useRequestHeaders(['cookie'])
 const {data, pending, error, refresh} = await useFetch(`${config.API_BASE_URL}/auth/user`, {
     onRequest({request, options}) {
         options.query = options.query || {}
-        options.headers = options.headers || {}
-        console.log("onRequest")
-        console.log(request)
+        options.headers = options.headers || {};
         // options.headers = options.headers || {}
     },
     onRequestError({request, options, error}) {
@@ -22,33 +20,42 @@ const {data, pending, error, refresh} = await useFetch(`${config.API_BASE_URL}/a
         // Process the response data
         console.log("onResponse")
         console.log(response)
+        console.log(response.data)
         console.log(options)
     },
-    onResponseError({request, response, options}) {
+    async onResponseError({request, response, options}) {
         // Handle the response errors
         console.log("onResponseError")
         console.log(response)
         console.log("response.error")
-        console.log(response.error)
-        // if (response.status === 401 || response.status === 403) {
-        //     // 새로운 액세스 토큰을 사용하여 요청을 재시도
-        //     // const newAccessToken = localStorage.getItem('AccessToken');
-        //     if (newAccessToken) {
-        //         console.log("재시도 로직")
-        //         // 헤더에 새 토큰을 설정
-        //         options.headers.Authorization = `Bearer ${newAccessToken}`;
-        //         // 요청을 다시 보내는 로직
-        //         refresh();
-        //     }
-        // }
+        console.log(response._data)
+        console.log("response.status")
+        console.log(response.status)
+        if (response.status === 401) {
+            if (response._data === "EXPIRED_ACCESS_TOKEN") {
+                const {
+                    data: refreshToken,
+                    pending: tokenPending,
+                    error: tokenError,
+                    refresh: tokenRefresh
+                } = await callApiWithAutoRefresh();
+                if (tokenError.value === "success") {
+                    console.log("==========refreshToken.value==========")
+                    console.log(refreshToken.value)
+                } else {
+                    /* alert 처리 */
+                }
+            }
+            // console.log(refreshToken.value)
+        }
     },
     method: "GET",
     headers: headers,
     credentials: 'include'
 })
 
-async function callApiWithAutoRefresh(url, options) {
-    const {data : refreshToken, pending: tokenPending, error: tokenError, refresh: tokenRefresh} =  await useFetch(`${config.API_BASE_URL}/auth/reissueToken`, {
+async function callApiWithAutoRefresh() {
+    return await useFetch(`${config.API_BASE_URL}/auth/reissueToken`, {
         onRequest({request, options}) {
             options.query = options.query || {}
             options.headers = options.headers || {}
@@ -77,6 +84,7 @@ async function callApiWithAutoRefresh(url, options) {
         headers: headers,
         credentials: 'include'
     });
+
 }
 
 
